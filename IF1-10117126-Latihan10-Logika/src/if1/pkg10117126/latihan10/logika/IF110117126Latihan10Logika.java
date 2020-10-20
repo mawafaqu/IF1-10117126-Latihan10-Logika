27df3cd67eb24c05ffd42723e2be8d53750f78e7
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:47:23 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan10-Logika/src/if1/pkg10117126/latihan10/logika/IF110117126Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan10.logika;
+
+/**
+ *
+ * @author Nama : Mawa Faqu Rochman
+ *         NIM  : 10117126
+ */
+public class IF110117126Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
